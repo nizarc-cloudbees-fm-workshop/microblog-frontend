fa85f1afab689583189e75cbdf8a799415b50560
Nizar Choudhury <31782356+nizarchoudhury@users.noreply.github.com>
2022-09-23 14:01:28 -0400

    Update flags.js

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -1,7 +1,8 @@
 import Rox from 'rox-browser'
 
 export const Flags = {
-  sidebar: new Rox.Flag(false)
+  sidebar: new Rox.Flag(false),
+  title: new Rox.flag(false)
 }
 
 async function initCloudBees () {
